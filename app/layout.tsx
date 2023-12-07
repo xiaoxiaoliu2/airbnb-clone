71f8fb7d53635c561e32c4b2cf1753e2db734f57
xiaoxiaoliu2 <l.margaret.liu@gmail.com>
2023-12-07 22:42:43 +0800

    Navbar UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 // import { Inter } from 'next/font/google'
 import { Nunito } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/navbar/Navbar'
 
 // const inter = Inter({ subsets: ['latin'] })
 const font = Nunito({ subsets: ['latin'] })
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar/>
         {children}
         </body>
     </html>
